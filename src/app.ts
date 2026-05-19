b0595c8b2df4e4dfbcfa76946d4b7a9e7221adac
theshameem <theshameem.alam@gmail.com>
2026-05-19 10:28:00 +0600

    fix: cors and global error handler

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,24 @@ import { userRouter } from "./modules/user/user.route";
 import { authRouter } from "./modules/auth/auth.router";
 import logger from "./middleware/logger";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 export const app: Application = express();
+
+const corsOptions = {
+  origin: "http://localhost:5000",
+};
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(logger);
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRouter); // Routes for user
 app.use("/api/profile", profileRouter); // Routes for profile
 app.use("/api/auth", authRouter); // Routes for auth
+
+app.use(globalErrorHandler);
